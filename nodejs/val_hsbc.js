893b57faa22303e836c1d989959005211688d869
Henry WKK <henry1987@gmail.com>
2019-01-21 17:30:28 +0800

    Updated val_hsbc.js

--- nodejs/val_hsbc.js
+++ nodejs/val_hsbc.js
@@ -1,20 +1,22 @@
+/* 
+HSBC Valuation site: https://www.hsbc.com.hk/zh-hk/personal/mortgages/property-valuation-tool.html
+*/
+
 
 module.exports.hsbc_val = function() {
     var request = require("request")
 
-    url = 'https://rbwm-api.hsbc.com.hk/pws-hk-hase-mortgage-eapi-prod-proxy/v1/evaluation/address/valuation'
+    var url = 'https://www.hsbc.com.hk/gpib/channel/proxy/mortgageSvc/enqPropVal'
 
-    payload = {
-            "area":"K", 
-            "district":"NTK", 
-            "estate":"AG", 
-            "block":"191", 
-            "floor":"0030",
-            "flat":"3",
-            "carpark":"",
-            "tcKnowledge":"on",
-            "hpContactUs":""
-        }
+    var payload = {
+        "locale":"zh-hk",
+        "zoneId":"2", //九龍
+        "districtId":"17", //牛頭角
+        "estateId":"263", //淘大花園
+        "blockId":"294,3621,nil", //I D 座
+        "floor":"35", 
+        "flat":"1"
+    }
 
     return new Promise(function(resolve, reject){
         request({uri: url, method: 'POST',json: payload}, (err, res, body) => {
