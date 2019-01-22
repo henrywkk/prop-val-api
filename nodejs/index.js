56df1690ba4988a4797e2076a1c31ee468fb96a1
Henry WKK <henry1987@gmail.com>
2019-01-22 17:51:55 +0800

    Update index.js
    add driver to val_hase.hase_all_district()

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,4 +1,8 @@
+const val_hase = require('./val_hase')
 const val_hsbc = require('./val_hsbc')
 
-val_hsbc.hsbc_val()
+//val_hsbc.hsbc_val()
 
+val_hase.hase_all_district()
+//val_hase.hase_all_estate()
+//val_hase.hase_all_area()
